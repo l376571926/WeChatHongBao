9c5d49b75c59f0b8cfba2c44711d7039cecc9b0b
l376571926 <15897461476@139.com>
2019-01-09 10:02:33 +0800

    解决图片消息一直弹窗的bug，精确识别红包消息

--- app/src/main/java/group/tonight/hongbao/HongBaoAccessibilityService.java
+++ app/src/main/java/group/tonight/hongbao/HongBaoAccessibilityService.java
@@ -4,6 +4,7 @@ import android.accessibilityservice.AccessibilityService;
 import android.text.TextUtils;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.Toast;
 
 import com.socks.library.KLog;
 
@@ -11,12 +12,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HongBaoAccessibilityService extends AccessibilityService {
+    //微信地球启动页
+    private static final String LAYOUT_SPLASH_PAGE = "com.tencent.mm.app.WeChatSplashActivity";
+    //新版微信第二启动页
+    private static final String LAYOUT_WHATS_NEW = "com.tencent.mm.plugin.whatsnew.ui.WhatsNewUI";
+    //微信首页
+    private static final String LAYOUT_WECHAT_HOME = "com.tencent.mm.ui.mogic.WxViewPager";
     //聊天列表
     private static final String LAYOUT_CHAT_LIST = "com.tencent.mm.ui.LauncherUI";
     //拆红包界面
     private static final String LAYOUT_OPEN_RED_BAG = "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyNotHookReceiveUI";
     //红包详情
     private static final String LAYOUT_RED_BAG_DETAIL = "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI";
+    //微信更新
+    private static final String LAYOUT_UPDATE_WECHAT = "com.tencent.mm.plugin.webview.ui.tools.WebViewUI";
 
     private String mCurrentActivityName;
     private boolean mOpenningRedBag;
@@ -33,10 +42,12 @@ public class HongBaoAccessibilityService extends AccessibilityService {
 
         AccessibilityNodeInfo rootInActiveWindow = getRootInActiveWindow();
         switch (eventType) {
-            case AccessibilityEvent.TYPE_VIEW_CLICKED://1
+            case AccessibilityEvent.TYPE_VIEW_CLICKED://1   视图被点击
+                break;
             case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED://2
             case AccessibilityEvent.TYPE_VIEW_SELECTED://4
-            case AccessibilityEvent.TYPE_VIEW_FOCUSED://8
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED://8   视图获得焦点
+                break;
             case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED://16
                 break;
             case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED://32
@@ -53,15 +64,29 @@ public class HongBaoAccessibilityService extends AccessibilityService {
                 } else if (LAYOUT_RED_BAG_DETAIL.equals(mCurrentActivityName)) {
                     KLog.e("红包详情打开");
                     mOpenningRedBag = false;
+
+                    if (getRootInActiveWindow() != null) {
+                        List<AccessibilityNodeInfo> moneyNodeInfo = getRootInActiveWindow().findAccessibilityNodeInfosByViewId("com.tencent.mm:id/cqv");//金额
+                        if (moneyNodeInfo != null) {
+                            if (!moneyNodeInfo.isEmpty()) {
+                                CharSequence text = moneyNodeInfo.get(0).getText();
+                                String tips = "抢到" + text + "元";
+                                KLog.e(tips);
+                                Toast.makeText(this, tips, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
                 }
                 break;
-            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED://64
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED://64    通知栏状态改变
+
+                break;
             case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER://128
             case AccessibilityEvent.TYPE_VIEW_HOVER_EXIT://256
             case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START://512
             case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END://1024
                 break;
-            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED://2048
+            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED://2048  窗口内容改变
                 if (mCurrentActivityName != null) {
                     if (LAYOUT_CHAT_LIST.equals(mCurrentActivityName)) {
                         if (mOpenningRedBag) {
@@ -93,13 +118,15 @@ public class HongBaoAccessibilityService extends AccessibilityService {
                         for (AccessibilityNodeInfo nodeInfo : rootInActiveWindow.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/k4")) {//返回
                             if (nodeInfo.isClickable()) {
                                 //手动返回
+                                nodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
                                 break;
                             }
                         }
                     }
                 }
                 break;
-            case AccessibilityEvent.TYPE_VIEW_SCROLLED://4096
+            case AccessibilityEvent.TYPE_VIEW_SCROLLED://4096   视图滚动
+                break;
             case AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED://8192
             case AccessibilityEvent.TYPE_ANNOUNCEMENT://16384
             case AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUSED://32768
@@ -135,17 +162,26 @@ public class HongBaoAccessibilityService extends AccessibilityService {
         }
         //每个聊天联系人item
         //红包布局，包含红包图标、恭喜发财、已被领完
-        //所有可点击的红包，包括已拆与未拆
-        List<AccessibilityNodeInfo> nodeInfoList = rootInActiveWindow.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/ao4");
-        //如果当前聊天窗口中没有红包消息，走这里
-        if (nodeInfoList.isEmpty()) {
+        //所有可点击的红包，包括已拆与未拆，还有纯图片消息
+        //筛选出仅包含“微信红包”标签的消息
+        List<AccessibilityNodeInfo> redBagNodeList = new ArrayList<>();
+        for (AccessibilityNodeInfo nodeInfo : rootInActiveWindow.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/ao4")) {
+            List<AccessibilityNodeInfo> nodeInfos = nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/apf");//白色微信红包标签
+            if (nodeInfos != null) {
+                if (!nodeInfos.isEmpty()) {
+                    redBagNodeList.add(nodeInfo);
+                }
+            }
+        }
+        if (redBagNodeList.isEmpty()) {
             KLog.e("当前聊天窗口中没有红包消息");
             return false;
         }
+        KLog.e("发现红包消息个数：" + redBagNodeList.size());
         //从最新消息往旧消息查找红包，查到未拆红包，立刻跳出循环
         mAvailableNodeInfosList.clear();
-        for (int i = nodeInfoList.size() - 1; i >= 0; i--) {
-            AccessibilityNodeInfo nodeInfo = nodeInfoList.get(i);//所有可点击的红包结点
+        for (int i = redBagNodeList.size() - 1; i >= 0; i--) {
+            AccessibilityNodeInfo nodeInfo = redBagNodeList.get(i);//所有可点击的红包结点
             List<AccessibilityNodeInfo> infoList = nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/ape");//看有没有“已被领完”结点的id存在
             if (infoList != null) {
                 if (infoList.isEmpty()) {
